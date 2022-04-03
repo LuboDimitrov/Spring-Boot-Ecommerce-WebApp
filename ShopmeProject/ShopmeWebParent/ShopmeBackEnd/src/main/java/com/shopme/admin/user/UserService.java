30d8311f33d79f47e7d766ceba88d3d625298160
LuboDimitrov <lubo9991@hotmail.com>
2022-04-03 18:22:13 +0200

    added save user function and list roles

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -1,5 +1,6 @@
 package com.shopme.admin.user;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,19 @@ public class UserService {
     @Autowired //to let Spring inject an instance at runtime (dependency injection)
     private UserRepository userRepo;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     public List<User> listAll() {
         return (List<User>) userRepo.findAll();
+    }
+
+    //to show roles in the new users form
+    public List<Role> listRoles() {
+        return (List<Role>) roleRepository.findAll();
+    }
+
+    public void saveUser(User user) {
+        userRepo.save(user);
     }
 }
